04a3bec296c04b2b484bb42f321d4f29f39d7716
Harshwardhankash <102742183+Harshwardhankash@users.noreply.github.com>
2022-04-13 21:45:42 +0530

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,291 @@
+import sys
+import time
+import os
+
+
+def union(f_1,f_2):
+    l1 = {}
+    l2={}
+    l3={}
+    """
+    Finding the union of two dictionaries
+
+    Args:
+    file_1 file_2 (dict):dictionaries to find union
+
+    Returns:
+    dictionaries of intersection of two dictionaries
+
+    """
+    for i in f_1:
+        if (i == None):
+            l1[i] = 1
+            l3[i]=1
+    for j in f_2:
+
+        l1[j]=1
+        l2[j]=1
+
+    return (len(l1)-len(l2)-len(l3))
+
+
+# def split(word):
+#     l1.update()
+#     return list(word)
+# def checkEmail(s):
+#     k=split(s)
+#     d=k[1]
+#     print(ord(d))
+#     count=0
+#     k1=0
+#     g=0
+#     gn=0
+#     for i in k:
+#         if(gn==1):
+#             if(ord(i) in range(48,57)):
+#                 return False
+#         if((g==0) and (k1==0)):
+#             if(ord(i) in range(48,57)):
+#                 return False
+#         if(g==3):
+#             if(ord(i) in range (48,57)):
+#                 return False
+#         if(ord(i) in range(97,122)or(ord(i) in range(65,90))):
+#             if(g==0) and (k1==0):
+#
+#                 k1=1
+#             continue
+#         if(ord(i)==45):
+#             if(ord[i+1] in range(97,122)):
+#                 continue
+#         if(ord(i)==64):
+#             g=3
+#             gn=1
+#             if(k1==1):
+#                 g = 2
+#                 if(k1==1):
+#                     continue
+#         if(ord(i)==46):
+#             count=count+1
+#             if((k1==1) and(g==2)):
+#                 continue
+#     if(k1==1)and(g==2):
+#         return True
+#     else:
+#         return False
+
+
+f = time.time()
+
+
+def validate_args(y):
+    if (os.path.exists(y)):
+        return (y)
+
+
+def checkEmail(s):
+    s = s.strip()
+    k = list(s)
+    count = 0
+    k1 = 0
+    g = 0
+    gn = 0
+    for i in k:
+        if (gn == 1):
+            if (ord(i) in range(48, 57)):
+                return False
+        if ((g == 0) and (k1 == 0)):
+            if (ord(i) in range(48, 57)):
+                return False
+        if (g == 3):
+            if (ord(i) in range(48, 57)):
+                return False
+        if (ord(i) in range(97, 122) or (ord(i) in range(65, 90))):
+            if (g == 0) and (k1 == 0):
+                k1 = 1
+            continue
+        if (ord(i) == 45):
+            continue
+        if (ord(i) == 64):
+            g = 3
+            gn = 1
+            if (k1 == 1):
+                g = 2
+                if (k1 == 1):
+                    continue
+        if (ord(i) == 46):
+            count = count + 1
+            if ((k1 == 1) and (g == 2)):
+                continue
+    if (k1 == 1) and (g == 2):
+        return True
+    else:
+        return False
+
+
+def read_file(l1_file):
+    """
+    Read the file line by line, check validity of the email, and returns a list of valid emails
+
+     Args:
+        li_file(str): Name of the file to read
+
+     Returns:
+        (dict): a collection of valid emails
+
+     """
+    hm = {}
+    with open(l1_file, 'r') as f2:
+        for email in f2.readlines():
+            if checkEmail(email):
+                hm[email] = "true"
+
+    return hm
+
+
+def write_file(result):
+    a = time.time()
+    print(str(sys.argv[1]) + " " + str(len(l1)) + " emails," + str(sys.argv[2]) + " " + str(
+        len(l2)) + " emails," + " " + str(sys.argv[3]) + " " + str(
+        result) + " emails; " + "Time taken: " + str(a - f) + " seconds")
+    # print(result)
+
+
+f = time.time()
+if __name__ == "__main__":
+    l1_file = validate_args(sys.argv[1])
+    l2_file = validate_args(sys.argv[2])
+    ret_file = validate_args(sys.argv[3])
+
+    # l2_file=validate_args(sys.argv[1])
+    # ret_file=validate_args(sys.argv[2])
+
+    l1 = read_file(l1_file)
+    l2 = read_file(l2_file)
+
+    result = union(l1,l2)
+    write_file(result)
+#
+#
+#
+# import sys
+# import time
+# import os.path
+#
+# l1={}
+# l2={}
+# def union(f_1,f_2):
+#
+#     s=len(f_1)
+#     d=len(f_2)
+#     for i in range(0,s):
+#         if (l1.get(f_1[i])==None):
+#             l1.update({s:i})
+#     for i in  range(0,d):
+#         if (l1.get(f_2[i])!=None and l2.get(d)== None ):
+#             l2.update({s:i})
+#     return(len(l1)-len(l2))
+#
+#
+#
+# def split(word):
+#     l1.update()
+#     return list(word)
+# def checkEmail(s):
+#
+#     for i in k:
+#         if(gn==1):
+#             if(ord(i) in range(48,57)):
+#                 return False
+#         if((g==0) and (k1==0)):
+#             if(ord(i) in range(48,57)):
+#                 return False
+#         if(g==3):
+#             if(ord(i) in range (48,57)):
+#                 return False
+#         if(ord(i) in range(97,122)or(ord(i) in range(65,90))):
+#             if(g==0) and (k1==0):
+#
+#                 k1=1
+#             continue
+#         if(ord(i)==45):
+#             if(ord[i+1] in range(97,122)):
+#                 continue
+#         if(ord(i)==64):
+#             g=3
+#             gn=1
+#             if(k1==1):
+#                 g = 2
+#                 if(k1==1):
+#
+#
+#
+#                     continue
+#         if(ord(i)==46):
+#             count=count+1
+#             if((k1==1) and(g==2)):
+#                 continue
+#     if(k1==1)and(g==2):
+#         return True
+#     else:
+#         return False
+#
+#
+#
+#
+# f = time.time()
+# def validate_args(argv):
+#
+#     if(os.path.exists(argv)):
+#         return (argv)
+#
+#
+# def read_file(l1_file):
+#
+#     fh = open(l1_file, "r")
+#     try:
+#         return l1_file.read()
+#     finally:
+#         l1_file.close()
+#
+#
+#
+#     for i in range(0,l1_file):
+#
+#         d11=checkEmail(emails[i])
+#         if(d11==True):
+#
+#             union(emails[i],i)
+#
+#
+#
+#
+# def read_file(l2_file):
+#
+#
+#     emails1=[]
+#
+#     with open("l2_file.txt", 'r') as f2:
+#         for email in f2.readlines():
+#             emails1.append(email)
+#         r2 = len(l2_file)
+#     for i in range(0, r2):
+#         hm3 = {emails1[i], i}
+#         d111 = checkEmail(emails1[i])
+#
+#         if (d111 == True):
+#
+#             union(emails1[i], i)
+#
+#
+# a=time.time()
+# def write_file(result):
+#     #print(str(sys.argv[1])+" "+str(r1)+" emails,"+str(sys.argv[2])+" "+str(r2)+" emails,"+" "+str(sys.argv[3])+" "+str(len(l1))+" emails; "+"Time taken: "+str(a-f)+" seconds")
+#     print(result)
+# def main():
+#     if __name__=="__main__":
+#         l1_file,l2_file,ret_file=validate_args(sys.argv)
+#         l1=read_file(l1_file)
+#         l2=read_file(l2_file)
+#         result=union(l1,l2)
+#         write_file(result)
--- union.py
+++ union.py
@@ -3,7 +3,7 @@ import time
 import os
 
 
-l1={}
+
 def union(f_1,f_2):
     """
     Finding the union of two dictionaries
@@ -15,99 +15,154 @@ def union(f_1,f_2):
     dictionaries of union of two dictionaries
 
     """
-    s=len(f_1)
-    d=len(f_2)
-    for i in range(0,s):
-        if (l1.get(f_1[i])==None):
-            l1.update({s:i})
-    for i in  range(0,d):
-        if (l1.get(f_2[i])==None):
-            l1.update({s:i})
+    l1 = {}
+    for i in f_1:
+        l1[i] = 1
+    for j in f_2:
+        l1[j] = 1
     return (l1)
 
 
 
-def split(word):
-    l1.update()
-    return list(word)
-def checkEmail(s):
-    k=split(s)
-    d=k[1]
-    print(ord(d))
-    count=0
-    k1=0
-    g=0
-    gn=0
-    for i in k:
-        if(gn==1):
-            if(ord(i) in range(48,57)):
-                return False
-        if((g==0) and (k1==0)):
-            if(ord(i) in range(48,57)):
-                return False
-        if(g==3):
-            if(ord(i) in range (48,57)):
-                return False
-        if(ord(i) in range(97,122)or(ord(i) in range(65,90))):
-            if(g==0) and (k1==0):
+# def split(word):
+#     l1.update()
+#     return list(word)
+# def checkEmail(s):
+#     k=split(s)
+#     d=k[1]
+#     print(ord(d))
+#     count=0
+#     k1=0
+#     g=0
+#     gn=0
+#     for i in k:
+#         if(gn==1):
+#             if(ord(i) in range(48,57)):
+#                 return False
+#         if((g==0) and (k1==0)):
+#             if(ord(i) in range(48,57)):
+#                 return False
+#         if(g==3):
+#             if(ord(i) in range (48,57)):
+#                 return False
+#         if(ord(i) in range(97,122)or(ord(i) in range(65,90))):
+#             if(g==0) and (k1==0):
+#
+#                 k1=1
+#             continue
+#         if(ord(i)==45):
+#             if(ord[i+1] in range(97,122)):
+#                 continue
+#         if(ord(i)==64):
+#             g=3
+#             gn=1
+#             if(k1==1):
+#                 g = 2
+#                 if(k1==1):
+#                     continue
+#         if(ord(i)==46):
+#             count=count+1
+#             if((k1==1) and(g==2)):
+#                 continue
+#     if(k1==1)and(g==2):
+#         return True
+#     else:
+#         return False
 
-                k1=1
+
+f = time.time()
+def validate_args(y):
+    if(os.path.exists(y)):
+        return (y)
+    
+def checkEmail(s):
+
+
+
+    s=s.strip()
+    k = list(s)
+    count = 0
+    k1 = 0
+    g = 0
+    gn = 0
+    for i in k:
+        if (gn == 1):
+            if (ord(i) in range(48, 57)):
+                return False
+        if ((g == 0) and (k1 == 0)):
+            if (ord(i) in range(48, 57)):
+                return False
+        if (g == 3):
+            if (ord(i) in range(48, 57)):
+                return False
+        if (ord(i) in range(97, 122) or (ord(i) in range(65, 90))):
+            if (g == 0) and (k1 == 0):
+                k1 = 1
             continue
-        if(ord(i)==45):
-            if(ord[i+1] in range(97,122)):
-                continue
-        if(ord(i)==64):
-            g=3
-            gn=1
-            if(k1==1):
+        if (ord(i) == 45):
+            continue
+        if (ord(i) == 64):
+            g = 3
+            gn = 1
+            if (k1 == 1):
                 g = 2
-                if(k1==1):
+                if (k1 == 1):
                     continue
-        if(ord(i)==46):
-            count=count+1
-            if((k1==1) and(g==2)):
+        if (ord(i) == 46):
+            count = count + 1
+            if ((k1 == 1) and (g == 2)):
                 continue
-    if(k1==1)and(g==2):
+    if (k1 == 1) and (g == 2):
         return True
     else:
         return False
 
 
-f = time.time()
-def validate_args(sys.argv):
-    if(os.path.exists(y)):
-        return (y)
-    
+def read_file(l1_file):
 
-def read_file(l1122_file):
-    emails1=[]
-    fl={}
+    """
+    Read the file line by line, check validity of the email, and returns a list of valid emails
 
-    with open(l1122_file, 'r') as f2:
+     Args:
+        li_file(str): Name of the file to read
+
+     Returns:
+        (dict): a collection of valid emails
+
+     """
+    hm = {}
+    with open(l1_file, 'r') as f2:
         for email in f2.readlines():
-            emails1.append(email)
-        r2 = len(l1122_file)
-        for i in range(0, r2):
-            hm3 = {emails1[i], i}
-            d111 = checkEmail(emails1[i])
+            if checkEmail(email):
+                hm[email]="true"
 
-            if (d111 == True):
-
-                fl.update(l2_file)
-            return (fl)
+    return hm
 
 
-a=time.time()
 def write_file(result):
-    #print(str(sys.argv[1])+" "+str(r1)+" emails,"+str(sys.argv[2])+" "+str(r2)+" emails,"+" "+str(sys.argv[3])+" "+str(len(l1))+" emails; "+"Time taken: "+str(a-f)+" seconds")
-    print(result)
+
+    a=time.time()
+
+    print(str(sys.argv[1]) + " " + str(len(l1)) + " emails," + str(sys.argv[2]) + " " + str(
+        len(l2)) + " emails," + " " + str(sys.argv[3]) + " " + str(
+        len(result)) + " emails; " + "Time taken: " + str(a - f) + " seconds")
+
+
+
+
+    # print(str(sys.argv[1])+" "+str(len(l1_file))+" emails,"+str(sys.argv[2])+" "+str(len(l2_file))+" emails,"+" "+str(sys.argv[3])+" "+str(len(result))+" emails; "+"Time taken: "+str(a-f)+" seconds")
+    # #print(result)
+f=time.time()
 if __name__=="__main__":
-    l1_file,l2_file,ret_file=validate_args(sys.argv)
+    l1_file=validate_args(sys.argv[1])
+    l2_file=validate_args(sys.argv[2])
+    ret_file=validate_args(sys.argv[3])
 
     # l2_file=validate_args(sys.argv[1])
     # ret_file=validate_args(sys.argv[2])
 
     l1=read_file(l1_file)
     l2=read_file(l2_file)
+
     result=union(l1,l2)
     write_file(result)
